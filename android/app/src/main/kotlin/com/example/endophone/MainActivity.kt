d6d870104f6ad89abc755bf3d27baeaeb6f16767
zainabalmousawi06 <zainabalmousawi067@gmail.com>
2026-08-15 01:46:09 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/endophone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.endophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 34, 89, 55),
+        appBar: AppBar(
+          
+
+        )
+      )
+    );
+     
+  }
+}
