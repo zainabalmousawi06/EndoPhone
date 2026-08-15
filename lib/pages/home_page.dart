f8d1dcac5787159ea7979f03e3666ebe510d2523
zainabalmousawi06 <zainabalmousawi067@gmail.com>
2026-08-15 23:29:02 +0300

    app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,28 +12,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 144, 205, 200),
-      appBar: AppBar(
-        
-      ),
+      backgroundColor: const Color.fromARGB(255, 144, 205, 172),
       body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
         slivers: [
-
+//
           SliverAppBar(
-            expandedHeight: 100,
-            collapsedHeight: 80,
+            expandedHeight: 150,
             stretch: true,
+            pinned: true,
+            backgroundColor:  Color.fromARGB(255, 243, 245, 239),
             flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color:  Color.fromARGB(255, 235, 160, 195),
+              ),
               title: Text(
                 'Endophone',
                 style: Theme.of(context).appBarTheme.titleTextStyle,
                 ),
-              
-
               centerTitle: true,
+              
             ),
           ),
-
+//
+        SliverToBoxAdapter(
+          child: Container(
+            height: 1000,
+            color: const Color.fromARGB(255, 91, 106, 59),
+          ),
+        )
           
         ],
 
